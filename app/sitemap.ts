45a6fc4f7b1feebdd6caa9f8efbace5459e4d118
SatiTech <www.satitechinfo@gmail.com>
2026-07-26 18:30:16 +0530

    Make sitemap metadata route static

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { pages } from "../data/site";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = "https://satitech-official.github.io/DS-Agro-website";
   return [
