5d7991b1a184329d7d3d34692755762c856361b7
SatiTech <www.satitechinfo@gmail.com>
2026-07-26 19:19:24 +0530

    Mount restored hero videography on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { HomeExperience } from "../components/HomeExperience";
+import { HeroVideoOverlay } from "../components/HeroVideoOverlay";
 
 export default function Home() {
-  return <HomeExperience />;
+  return <><HeroVideoOverlay /><HomeExperience /></>;
 }
