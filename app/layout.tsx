b012683b9f590de892d438574c289f3015d63e79
SatiTech <www.satitechinfo@gmail.com>
2026-07-26 19:46:17 +0530

    Load hero video styles without changing design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./hero-video.css";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 const siteUrl = "https://satitech-official.github.io/DS-Agro-website";
